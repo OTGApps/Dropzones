8cda9a2c2f01c133a77e6b5eba201d26ad6b462e
Mark Rickert <mjar81@gmail.com>
2020-05-13 19:18:12 -0400

    Remove a bunch more components

--- storybook/storybook-registry.ts
+++ storybook/storybook-registry.ts
@@ -1,6 +1,1 @@
 require("../app/components/speed-limit-sign/speed-limit-sign.story")
-require("../app/components/text/text.story")
-require("../app/components/button/button.story")
-require("../app/components/form-row/form-row.story")
-require("../app/components/checkbox/checkbox.story")
-require("../app/components/wallpaper/wallpaper.story")
