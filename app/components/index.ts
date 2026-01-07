f1dadb76f74454ab7dfa76a8b5a7000cefd94c8d
Mark Rickert <mjar81@gmail.com>
2026-01-07 11:33:40 -0700

    remove orphaned components

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,4 +1,3 @@
-export * from "./count-badge/count-badge"
 export * from "./dropzone-list-row/dropzone-list-row"
 export * from "./ParallaxScrollView"
 export * from "./speed-limit-sign/speed-limit-sign"
